5e7c342fb0cb02d40d8a32e53a85b5587147c447
ZoDSeR SyLVaN <zodser97@gmail.com>
2023-05-20 13:39:50 -0400

    JumpingOnClouds Problem

---
+++ JumpingOnTheClouds/Program.cs
@@ -0,0 +1,23 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return 
+     *  an INTEGER of minimum jumps.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<int> c)
+    {
+        int jumps = 0;
+        for(int i = 0; i < c.Count; jumps++)
+            if(i+2 < c.Count && c[i+2] != 1)
+                i+=2;
+            else
+                i+=1;
+        return jumps-1;
+    }
+
+}
