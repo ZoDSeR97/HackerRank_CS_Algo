f4027c51a2dc9849d8b6315b843079ae81def825
ZoDSeR SyLVaN <zodser97@gmail.com>
2023-05-20 13:11:03 -0400

    SalesByMatches Problem

---
+++ SalesByMatches/Program.cs
@@ -0,0 +1,28 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return 
+     *  an INTEGER of total pair of sock.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+        Dictionary<int, int> mem = new Dictionary<int, int>();
+        foreach(int num in ar)
+            if (!mem.ContainsKey(num))
+                mem[num] = 1;
+            else
+                mem[num] += 1;
+        int pairs = 0;
+        foreach(KeyValuePair<int, int> t in mem)
+            pairs += t.Value/2;
+        return pairs;
+    }
+
+}
